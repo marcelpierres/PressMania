c745cb968ff83c6bdc236e1c26ec011ed03d4fab
Marcel-Pierre Samuels <marcelpierre.s@gmail.com>
2019-03-12 23:42:26 -0400

    Routing and Redux

--- PressMania/App.js
+++ PressMania/App.js
@@ -9,27 +9,36 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import PressMania from './src/PressMania';
+import LoginScreen from './src/components/LoginScreen';
+import  store from './src/store';
+import {Provider} from 'react-redux';
 
-const instructions = Platform.select({
+/*const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+});*/
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider store ={store} >
+      <MainScreen/>
+      </Provider>
+      
     );
   }
 }
 
+const AppStackNavigator = createStackNavigator({
+  Login: LoginScreen,
+  Home: PressMania
+});
+const MainScreen = createAppContainer(AppStackNavigator);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -37,14 +46,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
---
+++ PressMania/src/components/LoginScreen.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Text, View ,StyleSheet,Button} from 'react-native';
+
+ class LoginScreen extends Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Button title="PLAY" onPress={() => this.props.navigation.navigate('Home')}>
+
+          </Button>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+  });
+
+export default LoginScreen;
